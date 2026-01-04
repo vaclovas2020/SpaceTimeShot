2b362a5eab5eeb51cd7dfd55a3c8c4b3fdb6d47b
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2026-01-04 15:03:39 +0200

    use window posdtion 0,0

--- SpaceTimeShotLauncher/SpaceTimeShotLauncher.cpp
+++ SpaceTimeShotLauncher/SpaceTimeShotLauncher.cpp
@@ -196,7 +196,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
     HWND hWnd = CreateWindowW(
         szWindowClass, szTitle,
         WS_OVERLAPPEDWINDOWCUSTOM,
-        CW_USEDEFAULT, 0,
+        0, 0,
         rc.right - rc.left,
         rc.bottom - rc.top,
         nullptr, nullptr, hInstance, nullptr);
