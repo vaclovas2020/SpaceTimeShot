cdd21fc90ffa737e5bcdca994609473dc71e9e4f
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2026-01-04 11:25:09 +0200

    fix resource.h ids

--- SpaceTimeShotLauncher/resource.h
+++ SpaceTimeShotLauncher/resource.h
@@ -5,7 +5,7 @@
 #define IDS_APP_TITLE                   101
 #define IDR_ACCELERATOR1                102
 #define IDI_ICON1                       105
-#define IDS_APP_CLASS                   110
+#define IDS_APP_CLASS                   106
 #define ID_ACCELERATOR_VK_LEFT          40001
 #define ID_ACCELERATOR_VK_RIGHT         40002
 #define ID_ACCELERATOR_VK_UP            40003
@@ -15,9 +15,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40006
 #define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_SYMED_VALUE           107
 #endif
 #endif
