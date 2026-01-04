46bb613b0f2fdf1e440170472aa94e699ea0d51a
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2026-01-04 17:07:34 +0200

    add debug flag D3D11_CREATE_DEVICE_DEBUG

--- SpaceTimeShotLauncher/SpaceTimeShotLauncher.cpp
+++ SpaceTimeShotLauncher/SpaceTimeShotLauncher.cpp
@@ -123,22 +123,38 @@ void ShowHRESULT(HWND hwnd, HRESULT hr, const wchar_t* context)
 // ------------------------------------------------------------
 bool CreateDevice()
 {
+    // Define initial flags (e.g., for Direct2D compatibility)
+    UINT creationFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+
+    #if defined(_DEBUG)
+        // Add the debug flag only for debug builds in 2026
+        creationFlags |= D3D11_CREATE_DEVICE_DEBUG;
+    #endif
+
     DXGI_SWAP_CHAIN_DESC scd{};
-    scd.BufferCount = 2;
+    scd.BufferCount = 2;                                    // Flip models require 2+
     scd.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
     scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    scd.OutputWindow = g_hWnd;
-    scd.SampleDesc.Count = 1;
+    scd.OutputWindow = g_hWnd;                              // Ensure this is not NULL
+    scd.SampleDesc.Count = 1;                               // Flip models DO NOT support MSAA
+    scd.SampleDesc.Quality = 0;                             // Must be 0
     scd.Windowed = TRUE;
     scd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
-    scd.BufferCount = 2;
+    scd.Flags = 0;                                          // Avoid experimental flags during init
+
+    D3D_FEATURE_LEVEL featureLevels[] = {
+        D3D_FEATURE_LEVEL_11_1,
+        D3D_FEATURE_LEVEL_11_0,
+        D3D_FEATURE_LEVEL_10_1,
+        D3D_FEATURE_LEVEL_10_0
+    };
 
     HRESULT hr = D3D11CreateDeviceAndSwapChain(
         nullptr,
         D3D_DRIVER_TYPE_HARDWARE,
         nullptr,
-        D3D11_CREATE_DEVICE_BGRA_SUPPORT,
-        nullptr, 0,
+        creationFlags,
+        featureLevels, ARRAYSIZE(featureLevels),
         D3D11_SDK_VERSION,
         &scd,
         &g_SwapChain,
@@ -425,6 +441,8 @@ void Update()
 // ------------------------------------------------------------
 void Render()
 {
+    if (!g_D2DTarget) return;
+
     g_D2DTarget->BeginDraw();
     g_D2DTarget->Clear(D2D1::ColorF(D2D1::ColorF::Black));
 
